0ecc4529016e8525fa30ddd1af18b262e7c21d75
Rajat Mishra <rajatmca45@gmail.com>
2019-10-19 21:45:15 +0530

    binary

--- javap/src/com/hydes/test/FindConsucutiveNumber.java
+++ javap/src/com/hydes/test/FindConsucutiveNumber.java
@@ -1,10 +1,29 @@
 package com.hydes.test;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class FindConsucutiveNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int r=0,q=0;
+        List<Integer> list= new ArrayList<>();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        while(n>0)
+        {
+        	n=n/2;
+        	r=n%2;
+        	list.add(r);
+        }
+        System.out.println(list);
+
+        scanner.close();
+    }
 
 }
