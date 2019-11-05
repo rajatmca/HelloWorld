bebf4b4d471b0cb55c1f4161b4a266f9fedaf900
rajatmca <46083079+rajatmca@users.noreply.github.com>
2019-11-05 17:15:49 +0530

    add one line in program

--- java practice Set/src/com/hydes/test/Welcome.java
+++ java practice Set/src/com/hydes/test/Welcome.java
@@ -1,10 +1,12 @@
 package com.hydes.test;
 
 public class Welcome {
+	// program to print hello world
 
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
 		System.out.println("rajat");
+		System.out.println("hi how r u");
 
 	}
 
